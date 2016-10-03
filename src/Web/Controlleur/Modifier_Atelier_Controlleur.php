d4317935675a033523b7f21e374ead7596ed5e10
Lassouani Sofiane <lassouani_sofiane@outlook.com>
2016-10-03 16:09:31 +0200

    une modif

--- src/Web/Controlleur/Modifier_Atelier_Controlleur.php
+++ src/Web/Controlleur/Modifier_Atelier_Controlleur.php
@@ -2,9 +2,10 @@
 <?php
 // On demande la liste des atelier au model
 include_once('../Model/Modifier_Atelier_Model.php');
+var_dump($_POST) ; 
 
 update_Atelier($_POST);
-echo $_POST['theme'] ;
+
 
 include_once('../Vue/Liste_Atelier_Vue.php');
 
