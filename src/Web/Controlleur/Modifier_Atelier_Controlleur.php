7e26a087dc4b89d84ebd6c58e9122cb4bfd0d518
Lassouani Sofiane <lassouani_sofiane@outlook.com>
2016-10-04 00:28:22 +0200

    tache T5 fonctionnelle

--- src/Web/Controlleur/Modifier_Atelier_Controlleur.php
+++ src/Web/Controlleur/Modifier_Atelier_Controlleur.php
@@ -2,10 +2,13 @@
 <?php
 // On demande la liste des atelier au model
 include_once('../Model/Modifier_Atelier_Model.php');
-var_dump($_POST) ; 
 
-update_Atelier($_POST);
+if(count($_POST)) {
+	update_Atelier($_POST);
+}
 
 
-include_once('../Vue/Liste_Atelier_Vue.php');
+header('Location:../Vue/Liste_Atelier_Vue.php');
+
+
 
--- src/Web/Model/Modifier_Atelier_Model.php
+++ src/Web/Model/Modifier_Atelier_Model.php
@@ -29,8 +29,8 @@ function update_Atelier( $arraydata )
 {
 
     global $conn_to_db;
-    /*
-
+    
+/*
     $ID=$arraydata['id'];
     $theme=$arraydata['theme'];
     $titre=$arraydata['titre_definitif'];
@@ -51,10 +51,42 @@ function update_Atelier( $arraydata )
 
 
      $sql = "UPDATE produits SET * = :titre, :theme, :type, :date_h, :remarque, :lboratoire, :lieu, :duree, :capacite, :inscription, :resume, :animateur_conferencier; :partenaires, :public_vise, :cont_ped WHERE id=:ID";
+*/
 
-        //$sql = "UPDATE `atelier` SET `titre_definitif`=$arraydata['titre'],`theme`=$arraydata['theme'],`type`=$arraydata['type'],`date_horaires`=$arraydata['date_horaires'],`remarque`=$arraydata['remarque'],`laboratoire`=$arraydata['laboratoire'],`lieu`=$arraydata['lieu'],`duree`=$arraydata['duree'],`capacite`=$arraydata['capacite'],`inscription`=$arraydata['inscription'],`resume`=resume,`animateur_conferencier`=$arraydata['animateur_conferencier'],`partenaires`=$arraydata['partenaires'],`public_vise`=$arraydata['public_vise'],`cont_ped`=$arraydata['cont_ped'] WHERE 'id'=$arraydata['id']";
-   */
-     $sql= " ";
+     /* gain de temp spéctaculaire avec cette methode */
+      /*$id = $arraydata['id'];
+      unset($arraydata['id']);
+      $setStatment = array();
+      foreach($arraydata as $key => $value) {
+        $setStatment[] = '`'.$key."` = '".addslashes($value)."'";
+      }
+      var_dump($setStatment);
+      $statment = implode(', ', $setStatment);
+
+      echo $statment;
+
+      $sql = "UPDATE atelier SET ".$statment."
+              WHERE id = ".$id; 
+      */
+
+      $sql = "UPDATE `atelier` SET `titre_definitif`= '".addslashes($arraydata['titre_definitif'])."',
+                  `theme`= '".addslashes($arraydata['theme'])."',
+                  `type`='".addslashes($arraydata['type'])."',
+                  `date_horaires`= '".addslashes($arraydata['date_horaires'])."',
+                  `remarque`= '".addslashes($arraydata['remarque'])."',
+                  `laboratoire`= '".addslashes($arraydata['laboratoire'])."',
+                  `lieu`= '".addslashes($arraydata['lieu'])."',
+                  `duree`= '".addslashes($arraydata['duree'])."',
+                  `capacite`= '".addslashes($arraydata['capacite'])."',
+                  `inscription`= '".addslashes($arraydata['inscription'])."',
+                  `resume`= '".addslashes($arraydata['resume'])."',
+                  `animateur_conferencier`= '".addslashes($arraydata['animateur_conferencier'])."',
+                  `partenaires`= '".addslashes($arraydata['partenaires'])."',
+                  `public_vise`='".addslashes($arraydata['public_vise'])."',
+                  `cont_ped`='".addslashes($arraydata['cont_ped'])."'
+                       WHERE id=".intval($arraydata['id']);
+   
+   
     $conn_to_db->query($sql) ;
     
 }
