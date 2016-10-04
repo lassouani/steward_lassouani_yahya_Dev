42a6e6f58125946fc680c5f77fc67b0ee0f3d994
ybachir <yahya.bachir@gmail.com>
2016-10-04 23:08:05 +0200

    Tache T3 est prete a etre utiliser signalez moi si il ya un pb

---
+++ src/Web/Controlleur/Modifier_Atelier_Controlleur.php
@@ -0,0 +1,14 @@
+
+<?php
+// On demande la liste des atelier au model
+include_once('../Model/Modifier_Atelier_Model.php');
+
+
+if(count($_POST)) {
+	update_Atelier($_POST);
+}
+
+
+header('Location:../Vue/Liste_Atelier_Vue.php');
+
+	header('Location: ../Vue/Liste_Atelier_Vue.php');
