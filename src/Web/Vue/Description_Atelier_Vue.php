4d860f70efc93871513566ebc369d3f8aef3b31e
Lassouani Sofiane <lassouani_sofiane@outlook.com>
2016-10-06 10:22:14 +0200

    modification de description.php

--- src/Web/Vue/Description_Atelier_Vue.php
+++ src/Web/Vue/Description_Atelier_Vue.php
@@ -102,8 +102,9 @@
                             </div>
 
                             <div class="col col-xs-12 text-right">
+							<a href="../Vue/Modifier_Atelier_Vue.php?id=<?php echo $_GET["id"];?>"> <input type="button" class="btn btn-default" name="Modifier "value="Modifier "/></a>
                                 <form method="POST" action='../Controlleur/Liste_Atelier_Controlleur.php'>
-                                    <button type="button" class="btn btn-default">Modifier</button>
+                                   
                                     <button type="submit" class="btn btn-default"  name="atelier_id" value="<?php echo $_GET["id"]; ?>" >Supprimer</button>
                                 </form>
                             </div>
